96f2b1e61091d7adeccc3ebd1da4c6de2894e9ef
PhoenixDev1223 <phoenixdev1223@gmail.com>
2023-07-17 06:50:24 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
+import * as React from 'react'
 import Link from "next/link"
 import { Mail, Loader2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Switch } from "@/components/ui/switch"
+import { Label } from "@/components/ui/label"
+import { Slider } from "@/components/ui/slider"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { useToast } from "@/components/ui/use-toast"
 
 const Home = () => {
+  const { toast } = useToast()
+
   return (
     <div className="m-5">
       <h1>Buttons</h1>
@@ -54,6 +67,41 @@ const Home = () => {
         <TabsContent value="account">Make changes to your account here.</TabsContent>
         <TabsContent value="password">Change your password here.</TabsContent>
       </Tabs>
+      <br />
+      <h1>Switch</h1>
+      <hr />
+      <div className="flex items-center space-x-2">
+        <Switch id="airplane-mode" />
+        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+      </div>
+      <br />
+      <h1>Slider</h1>
+      <hr />
+      <Slider defaultValue={[33]} max={100} step={1} />
+      <br />
+      <h1>Tooltip</h1>
+      <hr />
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>Hover</TooltipTrigger>
+          <TooltipContent>
+            <p>Add to library</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <br />
+      <h1>Toast</h1>
+      <hr />
+      <Button
+        onClick={() => {
+          toast({
+            title: "Scheduled: Catch up",
+            description: "Friday, February 10, 2023 at 5:57 PM",
+          })
+        }}
+      >
+        Show Toast
+      </Button>
       <br />
     </div>
   )
