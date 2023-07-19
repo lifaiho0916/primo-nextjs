94e98a5fc6dc5845c392cc3cb6ba1af509c2e09e
PhoenixDev1223 <phoenixdev1223@gmail.com>
2023-07-19 21:34:22 +0800

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,12 @@
 import * as React from 'react'
-<<<<<<< HEAD
-import Image from "next/image"
-import Link from "next/link"
 import HeroTitle from '@/components/pages/Home/HeroTitle'
 import "./globals.css";
+
 const Home = () => {
   return (
     <div className='flex justify-center items-center py-52'>
       <HeroTitle />
-      
-=======
-import "../app/globals.css";
-//Home Page
-import HeroTitle from '@/components/pages/Home/HeroTitle'
-
-const Home = () => {
-  return (
-    <div>
-      <HeroTitle />
->>>>>>> e61ebe8d927c391c1c07258cd36e95c4975c56d9
     </div>
-    
   );
 }
 export default Home;                                                      
